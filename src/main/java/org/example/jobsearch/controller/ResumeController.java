8bb423631cf15223591f86f580f22714c4a207e1
Asanov Temirlan <temirlan.asanov@alatoo.edu.kg>
2026-04-27 11:04:25 +0600

    feat: added 3 new HTTP methods for create (POST), update (PUT), delete (DELETE) resumes

--- src/main/java/org/example/jobsearch/controller/ResumeController.java
+++ src/main/java/org/example/jobsearch/controller/ResumeController.java
@@ -3,10 +3,9 @@ package org.example.jobsearch.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.jobsearch.dto.ResumeDto;
 import org.example.jobsearch.service.ResumeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,8 +21,26 @@ public class ResumeController {
         return resumeService.getAllResumes();
     }
 
+    @PostMapping
+    public HttpStatus createResume(@RequestBody ResumeDto resumeDto) {
+        resumeService.createResume(resumeDto);
+        return HttpStatus.CREATED;
+    }
+
+    @PutMapping("{resumeId}")
+    public ResponseEntity<ResumeDto> updateResume(@PathVariable Long resumeId, @RequestBody ResumeDto resumeDto) {
+        ResumeDto resume = resumeService.updateResume(resumeId, resumeDto);
+        return ResponseEntity.ok(resume);
+    }
+
     @GetMapping("{categoryId}")
     public List<ResumeDto> getResumeByCategoryId(@PathVariable Integer categoryId) {
         return resumeService.getResumesByCategoryId(categoryId);
+    }
+
+    @DeleteMapping("{resumeId}")
+    public HttpStatus deleteResume(@PathVariable Long resumeId) {
+        resumeService.deleteResume(resumeId);
+        return HttpStatus.OK;
     }
 }
