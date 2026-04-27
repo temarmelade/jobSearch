6e9e2d8d5ceb2ce3cf35124427a430826ec92af7
Asanov Temirlan <temirlan.asanov@alatoo.edu.kg>
2026-04-27 11:03:19 +0600

    fix: changed types of Date to avoid problems with empty dates

--- src/main/java/org/example/jobsearch/dao/mapper/VacancyMapper.java
+++ src/main/java/org/example/jobsearch/dao/mapper/VacancyMapper.java
@@ -3,13 +3,16 @@ package org.example.jobsearch.dao.mapper;
 import org.example.jobsearch.model.Vacancy;
 import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 public class VacancyMapper implements RowMapper<Vacancy> {
 
     @Override
     public Vacancy mapRow(ResultSet rs, int rowNum) throws SQLException {
+
         Vacancy vacancy = new Vacancy();
         vacancy.setId(rs.getLong("id"));
         vacancy.setName(rs.getString("name"));
@@ -19,8 +22,14 @@ public class VacancyMapper implements RowMapper<Vacancy> {
         vacancy.setAuthorId(rs.getInt("author_id"));
         vacancy.setExpFrom(rs.getInt("exp_from"));
         vacancy.setExpTo(rs.getInt("exp_to"));
-        vacancy.setUpdateDate(rs.getTimestamp("update_date") != null ? rs.getTimestamp("update_date").toLocalDateTime() : null);
-        vacancy.setCreatedDate(rs.getDate("created_date").toLocalDate());
+        vacancy.setActive(rs.getBoolean("is_active"));
+
+        Timestamp updated = rs.getTimestamp("update_date");
+        vacancy.setUpdateDate(updated != null ? updated.toLocalDateTime() : null);
+
+        Date created = rs.getDate("created_date");
+        vacancy.setCreatedDate(created != null ? created.toLocalDate() : null);
+
         return vacancy;
     }
 
